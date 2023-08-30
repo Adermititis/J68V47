0a35d991e4f25456537675038bb3895ad8319934
30134007 <30134007@nescol.ac.uk>
2023-08-30 12:00:39 +0100

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * my first program
+ * @author Adam Christie
+ * @version 1
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }//writes out Hello world to the console
+}
